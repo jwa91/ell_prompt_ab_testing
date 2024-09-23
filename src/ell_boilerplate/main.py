f0cc7d3a4df5adcdd0446ddb1809f72008af18da
jwa91 <122170013+jwa91@users.noreply.github.com>
2024-09-23 21:19:35 +0200

    v2

--- src/ell_boilerplate/main.py
+++ src/ell_boilerplate/main.py
@@ -1,7 +1,34 @@
 # src/ell_boilerplate/main.py
+from dotenv import load_dotenv
+import os
 import ell
-from ell_boilerplate.config import EllClient
+import openai
+import anthropic
 from typing import List
+
+def initialize_ell():
+    load_dotenv()
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+    anthropic_api_key = os.getenv("ANTHROPIC_API_KEY")
+
+    if not openai_api_key:
+        raise EnvironmentError("OPENAI_API_KEY niet ingesteld")
+    if not anthropic_api_key:
+        raise EnvironmentError("ANTHROPIC_API_KEY niet ingesteld")
+
+    openai_client = openai.Client(api_key=openai_api_key)
+    anthropic_client = anthropic.Client(api_key=anthropic_api_key)
+
+    ell.init(verbose=True, store="./logdir", autocommit=True)
+    ell.config.register_model("gpt-4o", openai_client)
+    ell.config.register_model("gpt-4-turbo", openai_client)
+    ell.config.register_model("gpt-4o-mini", openai_client)
+    ell.config.register_model("claude-3-haiku-20240307", anthropic_client)
+    ell.config.register_model("claude-3-5-sonnet-20240620", anthropic_client)
+
+    print("ell is succesvol geconfigureerd met OpenAI en Anthropic clients.")
+
+initialize_ell()
 
 @ell.simple(model="gpt-4o-mini", temperature=1.0)
 def generate_story_ideas(about : str):
@@ -20,20 +47,13 @@ def choose_the_best_draft(drafts : List[str]):
 
 @ell.simple(model="gpt-4-turbo", temperature=0.2)
 def write_a_really_good_story(about : str):
-    """You are an expert novelist that writes in the style of Hemmingway. You write in lowercase."""
+    """You are an expert novelist that writes in the style of Shakespeare. You write in lowercase."""
     ideas = generate_story_ideas(about, api_params=(dict(n=4)))
-
     drafts = [write_a_draft_of_a_story(idea) for idea in ideas]
-
     best_draft = choose_the_best_draft(drafts)
-
-
     return f"Make a final revision of this story in your voice: {best_draft}."
 
 def main():
-    EllClient.get_instance().initialize()
-
-    # Gebruik ell functies
     story = write_a_really_good_story("a dog")
     print(story)
 
